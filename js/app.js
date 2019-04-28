9d9342ded8c90f8d3f1f9da4d2a5f7bb2eb118aa
Luis Rosario <luisrosariomix@gmail.com>
2019-04-27 17:37:11 -0700

    Created html elements in app.js using jquery. Also created html5 audio players in app.js using jquery for later use.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,77 @@
-console.log($)
+/////////////////////////////////////
+/// Lets create the Front End UI //
+//////////////////////////////////
+
+  //////////////////////////
+  /// Creating Header /////
+  ////////////////////////
+  const createUI = (numOfSlices) => {
+  $(`<div>`).addClass(`flex-head`).addClass(`head-left`).text(`Food Fortune`)
+  .appendTo($(`<div>`).addClass(`flex-header`).appendTo(`header`))
+
+  $(`<div>`).addClass(`flex-head`).addClass(`head-mid`).appendTo(`.flex-header`)
+  $(`<div>`).addClass(`flex-head`).addClass(`head-right`).appendTo(`.flex-header`)
+
+  /////////////////////////////////////////////
+  /// Creating Main & Container Elements /////
+  ///////////////////////////////////////////
+
+  $(`<main>`).appendTo(`body`)
+  $(`<div>`).addClass(`city`).appendTo(`main`)
+  $(`<div>`).addClass(`flex-container`).appendTo(`main`)
+
+  ////////////////////////////////////////////
+  /// Lets Begin Constructing Our Wheel /////
+  //////////////////////////////////////////
+
+  //Creating ul element that will container our inner wheel
+  $(`<ul>`).attr(`id`, `inner`).addClass(`the-wheel`).appendTo(`main .flex-container`)
+  //Looping here to create the inner wheel elements
+  //That we will construct into selectable slices
+  for(let i = 0; i < numOfSlices; i++) {
+    $(`<li>`).appendTo(`#inner`)
+    const $selectLiTag = $('li').eq(i)
+    $(`<div>`).addClass(`wheel-slice`).text(`${i}`).appendTo($selectLiTag)
+  }
+  //Create center of circle elements
+  $(`<div>`).addClass(`circle-button`).appendTo(`#inner`)
+  $(`<div>`).addClass(`circle-button-small`).appendTo(`#inner`)
+  $(`<div>`).addClass(`circle-button-tiny`).appendTo(`#inner`)
+
+  //Create our orb pop up for restaurant display
+  $(`<div>`).addClass(`orb`).appendTo(`#inner`)
+  //Create our outter wheel element
+  $(`<ul>`).addClass(`the-wheel`).addClass(`outter-wheel`).appendTo(`.flex-container`)
+
+  ///////////////////////////////////
+  /// Creating Footer Elements /////
+  /////////////////////////////////
+
+  $(`<div>`).addClass(`flex-foot`).addClass(`foot-left`)
+    .appendTo( $(`<div>`).addClass(`flex-footer`).appendTo($(`<footer>`).appendTo(`body`)))
+
+  $(`<div>`).addClass(`flex-foot`).addClass(`foot-mid`).appendTo(`.flex-footer`)
+  $(`<div>`).addClass(`flex-foot`).addClass(`foot-right`).appendTo(`.flex-footer`)
+
+  ////////////////////////////////////////////////
+  /// Creating Audio Players For Music & FX /////
+  //////////////////////////////////////////////
+
+  //Creating Music Player
+  $(`<audio controls loop>`).attr(`id`, `fortuneMusic`).css(`display`, `none`)
+    .attr(`src`, `audio/file_example_MP3_700KB.mp3`)
+    .attr(`id`, `musicMain`).attr(`type`, `audio/mpeg`)
+    .appendTo(`.foot-left`)
+
+  //Creating FX Player
+  $(`<audio controls>`).attr(`id`, `wheelFx`).css(`display`, `none`)
+    .attr(`src`, `audio/bleep.mp3`)
+    .attr(`id`, `bleepFx`).attr(`type`, `audio/mpeg`)
+    .appendTo(`.foot-left`)
+}
+
+$(() => {
+
+createUI(12)
+
+})
