0332a8428b266ada75b0dfc103bbbcd6c970148e
Luis Rosario <luisrosariomix@gmail.com>
2019-09-09 21:19:38 -0700

    Adjusted middleware

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3001
 
 app.use(express.static(`public`))
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/images', express.static(__dirname + '/public/images'));
 
 app.get(`/`, (req,res) => {
 
