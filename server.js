1ed235d1b5de9132acff159850d00dc6c2ee9ba5
Luis Rosario <luisrosariomix@gmail.com>
2019-09-09 21:23:47 -0700

    Adjusted middleware

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const app = express()
 const port = 3001
 
 app.use(express.static(`public`))
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
 
 app.get(`/`, (req,res) => {
 
 })
+
 app.listen(port, () => {
   console.log(`I'm listening to port ${ port }`)
 })
